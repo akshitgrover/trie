58b2b364a85aa6191012db2efe5933880d18290f
Akshit Grover <akshit.grover2016@gmail.com>
2019-05-02 13:37:17 +0530

    Trie: Add constructor for string type key

--- trie.h
+++ trie.h
@@ -1,10 +1,12 @@
+#include <string>
+
 #include "_trie_node.h"
 
 template <typename T>
 class trie {
     public:
         trie();
-    
+        void insert(std::string, T);
     private:
         tnode<T>* root;
         int size;
