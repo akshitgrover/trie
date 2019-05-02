732da14e19f9c118b07fa84eed1704d3b7a38d38
Akshit Grover <akshit.grover2016@gmail.com>
2019-05-02 14:19:45 +0530

    Trie: Add insert member function

--- trie.hpp
+++ trie.hpp
@@ -1,4 +1,27 @@
+#include <string>
+
+#include "_trie_util.h"
+
 template <typename T>
-trie<T>::trie(): size(0){
-    root = nullptr;
+trie<T>::trie(): size(0) {
+    T flag;
+    root = new tnode<T>(flag);
+}
+
+template <typename T>
+void trie<T>::insert(std::string key, T val) {
+  tnode<T>* node = this->root;
+  int ascii;
+  sIter(key, it) {
+    ascii = (int)*it;
+    if (node->getChild(ascii) != nullptr) {
+      node = node->getChild(ascii);
+    } else {
+      T flag;
+      tnode<T>* _node = new tnode<T>(flag);
+      node->addChild(_node, ascii);
+      node = node->getChild(ascii);
+    }
+  }
+  node->update(val);
 }
