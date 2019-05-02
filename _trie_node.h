e280fb930f043a84797571f3145683d271828231
Akshit Grover <akshit.grover2016@gmail.com>
2019-05-02 14:08:31 +0530

    TrieNode: Add getChild member function

--- _trie_node.h
+++ _trie_node.h
@@ -5,9 +5,9 @@ class tnode {
     public:
         explicit tnode(T v, bool eow = false);
         void addChild(tnode* child, char key);
+        tnode<T>* getChild(char key);
     private:
         T mapped_value;
-        char key;
         bool isEnd;
         std::vector <tnode*> children;
 };
@@ -16,11 +16,15 @@ template <typename T>
 tnode<T>::tnode(T val, bool eow) {
     this->mapped_value = val;
     this-> isEnd = eow;
-    std::vector <tnode*> ch(256, nullptr);
-    this->children = ch;
+    this->children = *(new std::vector <tnode<T>*>(256, nullptr));
 }
 
 template <typename T>
 void tnode<T>::addChild(tnode* child, char key) {
     this->children[(int)key] = child;
+}
+
+template <typename T>
+tnode<T>* tnode<T>::getChild(char key) {
+    return this->children[(int)key];
 }
